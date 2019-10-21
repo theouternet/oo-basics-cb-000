eec9a5c6f7bead6b7eab10cf07a0cc60df1792ee
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-10-21 03:25:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -20,4 +20,26 @@ class Shoe
     @color 
   end
   
+  def size=(size)
+    @size = size
+  end
+  
+  def size
+    @size
+  end
+  
+  def material=(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  def condition=(condition)
+    @condition = condition
+  end
+  def condition
+    @condition
+  end
+  
 end
