b7b00c61c6de33c6fbbf0c5fa26dd25444f35b33
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-10-21 02:55:27 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
-
-
+class Book 
+  
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+  
+  def title=(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+   def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+end 
